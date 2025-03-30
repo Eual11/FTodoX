002da2ebad9495785fb5c6ce62f1ddcff7cb5eef
Eual11 <uchihaeual11@gmail.com>
2025-03-30 23:23:54 +0300

    rename test to main

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,12 @@
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/component_base.hpp>
 #include <ftxui/component/screen_interactive.hpp>
-#include <iostream>
 int main(void) {
 
   auto todoui = ftxui::Make<todoUi>();
-
   auto scr = ftxui::ScreenInteractive::Fullscreen();
-
+  auto exitFunc =   scr.ExitLoopClosure();
+  todoui->setExitFunction(exitFunc);
   scr.Loop(todoui);
-  std::cout << "Hello Cruel World";
   return 0;
 }
